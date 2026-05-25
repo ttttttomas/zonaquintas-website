2c99d1f18631790805ee8833281eb434ae8bbd2d
Tomas Barajas <156537417+ttttttomas@users.noreply.github.com>
2026-05-25 15:52:12 -0300

    Integrate Facebook Meta Pixel for tracking
    Added Facebook Meta Pixel tracking script and noscript tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { FiltersProvider } from "./context/ContextFilters";
 import { SearchProvider } from "./context/SearchContext";
 import { UserProvider } from "./context/UserContext";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export const metadata: Metadata = {
   },
 };
 
+const META_PIXEL_ID = "1713121929685672";
 
 export default function RootLayout({
   children,
@@ -33,10 +35,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+            <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <FiltersProvider>
         <SearchProvider>
           <UserProvider>
             <body className={`bg-[#f7f3f0] mt-32 ${inter.className}`}>
+              <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
               <Toaster position="top-center" />
               <Header />
               {children}
