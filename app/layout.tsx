2cd72f0822c4a0c5e219099f374d4e0da5037a3a
Tomas Barajas <156537417+ttttttomas@users.noreply.github.com>
2026-05-31 20:02:40 -0300

    Integrate Google Tag Manager in layout
    Added Google Tag Manager script for tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,15 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MC25527K');
+          `}
+        </Script>
       </head>
       <FiltersProvider>
         <SearchProvider>
@@ -77,6 +86,14 @@ export default function RootLayout({
         gtag('config', 'G-1V563PGQMG');
       `}
     </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MC25527K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
               <Toaster position="top-center" />
               <Header />
               {children}
